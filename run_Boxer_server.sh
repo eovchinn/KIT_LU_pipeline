8fcd677526102b6f80edd94fac74bfabe2523222
eovchinn <e.ovchinnikova@gmail.com>
2015-03-09 13:52:49 +0100

    SOAP server as env var

--- run_Boxer_server.sh
+++ run_Boxer_server.sh
@@ -4,6 +4,6 @@
 # $ ./run_Boxer_server.sh
 
 CANDC_SERVER_BIN=$BOXER_DIR/bin/soap_server
-CANDC_SERVER_OPT="--models $BOXER_DIR/models/boxer --server localhost:9000 --candc-printer boxer"
+CANDC_SERVER_OPT="--models $BOXER_DIR/models/boxer --server $BOXER_SOAP_SERVER --candc-printer boxer"
 
 $CANDC_SERVER_BIN $CANDC_SERVER_OPT > /dev/stdout
