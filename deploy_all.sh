b34c9efb2556237fba45b2a87aa63fc83f8d770e
eovchinn <e.ovchinnikova@gmail.com>
2015-03-22 22:35:15 -0700

    adding env vars fixed

--- deploy_all.sh
+++ deploy_all.sh
@@ -5,8 +5,8 @@ source ${PWD}/lupipeline.setenv.sh
 
 read -p "Are you ok with adding LU pipeline env variables to your .bashrc? [yn]" answer
 if [[ $answer = y ]] ; then
-  echo 'export KIT_LU_DIR=${PWD}' >> ~/.bashrc
-  echo 'source ./lupipeline.setenv.sh' >> ~/.bashrc
+  echo "export KIT_LU_DIR=${PWD}" >> ~/.bashrc
+  echo "source ./lupipeline.setenv.sh" >> ~/.bashrc
 fi
 
 cd boxer
