45076cd9ec01559e52f0e2f7cdabc32913f9ae36
eovchinn <e.ovchinnikova@gmail.com>
2015-03-23 19:48:47 -0700

    setting env vars fixed

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo -e "#!/bin/bash\n\n" > lupipeline.setenv.sh
+echo -e "#!/bin/bash\n" > lupipeline.setenv.sh
 
 export KIT_LU_DIR=${PWD}
-echo -e "export KIT_LU_DIR=${PWD}\n" >> lupipeline.setenv.sh
+echo "export KIT_LU_DIR=${PWD}" >> lupipeline.setenv.sh
 
 export BOXER_DIR=$KIT_LU_DIR/boxer
-echo -e "BOXER_DIR=$KIT_LU_DIR/boxer\n" >> lupipeline.setenv.sh
+echo "export BOXER_DIR=$KIT_LU_DIR/boxer" >> lupipeline.setenv.sh
 
 export BOXER_SOAP_SERVER=localhost:9000
-echo -e "BOXER_SOAP_SERVER=localhost:9000\n" >> lupipeline.setenv.sh
+echo "export BOXER_SOAP_SERVER=localhost:9000" >> lupipeline.setenv.sh
 
 export HENRY_DIR=/usr/share/henry
-echo -e "HENRY_DIR=/usr/share/henry\n" >> lupipeline.setenv.sh
+echo "export HENRY_DIR=/usr/share/henry" >> lupipeline.setenv.sh
 
 export GRB_LICENSE_FILE=$KIT_LU_DIR/gurobi/gurobi.lic
-echo -e "GRB_LICENSE_FILE=$KIT_LU_DIR/gurobi/gurobi.lic" >> lupipeline.setenv.sh
+echo "export GRB_LICENSE_FILE=$KIT_LU_DIR/gurobi/gurobi.lic" >> lupipeline.setenv.sh
 
 read -p "Are you ok with adding LU pipeline env variables to your .bashrc? [yn]" answer
 if [[ $answer = y ]] ; then
